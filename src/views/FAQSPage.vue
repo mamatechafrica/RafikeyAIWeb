13db3d72a64eae66a992263618559b584f2a679c
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-18 00:34:29 +0300

    add image

--- src/views/FAQSPage.vue
+++ src/views/FAQSPage.vue
@@ -3,6 +3,14 @@
 </script>
 
 <template>
+  <div class="h-screen snap-y snap-mandatory overflow-y-scroll hide-scrollbar">
+    <section class="h-screen snap-start flex items-center ">
+      <div class="w-4/12 mx-auto">
+        <img  src="@/assets/images/under-const.png" alt="under-constuction-image"/>
+      </div>
+
+    </section>
+  </div>
 
 </template>
 
