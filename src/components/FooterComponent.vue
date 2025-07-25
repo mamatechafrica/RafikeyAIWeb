cf710377c38c34042aece56d455eaded84b34cd6
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-25 17:24:25 +0300

    add images

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,32 +1,47 @@
 <script setup lang="ts">
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { nextTick, onMounted } from 'vue'
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({
+  duration: 3,
+  ease: 'none'
+})
+
+onMounted(async ()=>{
+  await nextTick()
+
+})
 </script>
 
 <template>
-  <div class="h-screen snap-y snap-mandatory">
-    <section class="h-screen snap-start flex items-center w-full">
-      <div class="">
+  <div class="h-screen  w-full mx-auto">
+    <section class="panel w-10/12  h-screen flex justify-center md:items-center mx-auto">
+      <div class="hidden lg:block">
         <img src="@/assets/images/footer.png" alt="footer-image" class="w-full" />
+      </div>
+      <div class="lg:hidden w-full flex justify-center items-center">
+        <img src="@/assets/images/footer-small.png" alt="footer-small-image" class="" />
       </div>
     </section>
 
-    <section class="h-screen snap-start flex items-center justify-center">
-      <div class="grid grid-cols-4 gap-4 max-w-7xl w-full">
-        <div class="space-y-6 flex flex-col items-center col-span-2 ">
-          <h2 class="text-4xl text-shark-950 font-bold">RAFIKey</h2>
-          <div
-            class="border-b border-2 w-48 border-gradient-to-r from-coral-red-500 to-azure-radiance-600"
-          ></div>
-          <div class="w-3/6">
-            <p class="text-xl text-black text-center">
+    <section class="panel h-screen flex bg-white items-center justify-center">
+      <div class="grid grid-cols-4 gap-20 max-w-7xl w-full mx-auto">
+        <div class="space-y-6 flex flex-col items-center lg:col-span-2 col-span-4  ">
+          <h2 class="text-3xl lg:4xl text-shark-950 font-bold">RAFIKey</h2>
+          <div class="border-b border-2 w-48 border-gradient-to-r from-coral-red-500 to-azure-radiance-600"></div>
+          <div class="w-3/6 w">
+            <p class="text-sm lg:text-lgtext-black text-center">
               Your Safe Space for Sexual and Reproductive Health and Rights Information and services
             </p>
           </div>
         </div>
 
-        <div class="space-y-32 col-span-1">
-          <div class="flex flex-col">
-            <div>
-              <p class="text-lg text-stone-400">Rafikey</p>
+        <div class="lg:space-y-32 space-y-20 lg:col-span-1 col-span-2 flex flex-col items-center">
+          <div class="flex flex-col text-sm lg:text-lg">
+            <div class="text-nowrap">
+              <p class=" text-stone-400">Rafikey</p>
               <ul class="space-y-2">
                 <li>About</li>
                 <li>ChatBot</li>
@@ -34,9 +49,9 @@
               </ul>
             </div>
           </div>
-          <div class="flex flex-col col-span-1 ">
+          <div class="flex flex-col col-span-1 text-nowrap text-sm lg:text-lg ">
             <div>
-              <p class="text-lg text-stone-400">Contact</p>
+              <p class=" text-stone-400">Contact</p>
               <ul class="space-y-2">
                 <li>Tech Support</li>
                 <li>Data Privacy</li>
@@ -45,21 +60,21 @@
             </div>
           </div>
         </div>
-        <div class="space-y-32">
+        <div class="space-y-32 text-nowrap text-sm lg:text-lg ">
           <div class="flex flex-col ">
             <div>
-              <p class="text-lg text-stone-400">Our Research</p>
+              <p class="text-sm lg:text-lg text-stone-400">Our Research</p>
               <ul class="space-y-2">
                 <li>Process</li>
                 <li>Timeline</li>
               </ul>
             </div>
           </div>
-          <div class="flex flex-col ">
+          <div class="flex flex-col text-nowrap text-sm lg:text-lg ">
             <div>
-              <p class="text-lg text-stone-400">Privacy and Safety</p>
+              <p class="text-sm lg:text-lg text-stone-400">Privacy and Safety</p>
               <ul class="space-y-2">
-                <li>Terma of Use</li>
+                <li>Terms of Use</li>
                 <li>Privacy Policy</li>
               </ul>
             </div>
@@ -71,4 +86,13 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.hide-scrollbar {
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+}
+
+.hide-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+</style>
