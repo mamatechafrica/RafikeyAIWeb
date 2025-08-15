8156b42d16cad5475552d0aa61b2492ab68a1442
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-08-15 16:53:00 +0300

    add styling

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -38,29 +38,29 @@ const registerHandler = () => {
 
 <template>
 
-  <div class="fixed hidden lg:block left-0 right-0 top-0 bg-white z-50">
-    <div class="flex bg-white justify-between py-4 px-8  gap-4 ">
+  <div class="fixed hidden lg:block left-0  h-[102px] right-0 top-0 bg-white z-50">
+    <div class="flex bg-white items-center h-full justify-between py-4 px-[80px] gap-4 ">
 <!--      icon-->
-      <div class="cursor-pointer" @click="homeHandler">
+      <div class="cursor-pointer flex items-end pt-1 " @click="homeHandler">
         <img src="@/assets/images/RAFIKey.png" alt="rafikey-name-image">
       </div>
 <!--      navs-->
-      <div class="flex gap-10 ">
+      <div class="flex xl:gap-10 gap-4  ">
         <div v-for="tab in tabStore.getTabs" :key="tab.value">
           <button @click="handleRouteChange(tab.value)">
-            <span class="text-xl font-semibold" :class="[tabStore.activeTab === tab.value? 'text-black': 'text-gray-400']">{{ tab.name }}</span>
+            <span class="text-[24px] font-pt-sans-bold" :class="[tabStore.activeTab === tab.value? 'text-darkGrayOne': 'text-gray-400']">{{ tab.name }}</span>
           </button>
         </div>
       </div>
 <!--      buttons-->
-      <div class="flex gap-4">
+      <div class="flex h-[47px] w-[249px] gap-[53px]">
         <button @click="loginHandler">
-          <span class="text-xl font-semibold text-gray-400">Log In</span>
+          <span class="font-pt-sans-bold text-lightGrayTwo text-[24px] text-nowrap">Log In</span>
         </button>
         <button
           @click="registerHandler"
-          class="btn btn-sm bg-black drop-shadow-2xl">
-          <span class="text-lg py-3  font-semibold text-white">Register</span>
+          class="btn btn-sm h-full backdrop-blur-sm   shadow-[0_0_50px_0px] shadow-navBarShadowBackground/50 border-none  bg-darkGrayOne rounded-[8px] py-[8px] px-[28px]">
+          <span class="font-pt-sans-bold text-white text-[24px]">Register</span>
         </button>
       </div>
     </div>
@@ -84,7 +84,7 @@ const registerHandler = () => {
       :show="!webStore.collapseSidebarSmall"
       class="lg:hidden block"
     >
-      <Dialog class="relative z-10" @close="webStore.setCollapseSidebarSmall(true)">
+      <Dialog class="relative z-50" @close="webStore.setCollapseSidebarSmall(true)">
         <div class="fixed inset-0 overflow-hidden">
           <div class="absolute inset-0 overflow-hidden">
             <div class="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10 sm:pl-16">
@@ -134,8 +134,8 @@ const registerHandler = () => {
                     <div class="relative mt-6 ms-6 flex-1">
                       <!-- Your content -->
                       <div v-for="tab in tabStore.getTabs" :key="tab.value">
-                        <div @click.stop="handleRouteChange(tab.value)" class="ps-4" :class="[tabStore.activeTab === tab.value? 'bg-shark-950 w-full': '']">
-                          <span class="text-lg " :class="[tabStore.activeTab === tab.value? 'text-white': 'text-stone-400']">{{ tab.name }}</span>
+                        <div @click.stop="handleRouteChange(tab.value)" class="ps-4 cursor-pointer hover:bg-shark-100" :class="[tabStore.activeTab === tab.value? 'bg-shark-950 w-full': '']">
+                          <span class="font-urbanist text-[16px] " :class="[tabStore.activeTab === tab.value? 'text-white': 'text-stone-400']">{{ tab.name }}</span>
                         </div>
                       </div>
                       <div class="absolute bottom-0 w-full cursor-pointer">
@@ -143,13 +143,13 @@ const registerHandler = () => {
                           @click="loginHandler"
                           class="flex gap-4 hover:bg-shark-100 p-2">
                           <span class="material-icons-outlined">login</span>
-                          <span>Login</span>
+                          <span class="font-urbanist text-[16px]">Login</span>
                         </div>
                         <div
                           @click="registerHandler"
                           class="flex gap-4 hover:bg-shark-100 p-2 rounded">
                           <span class="material-icons-outlined text-shark-950">person_add</span>
-                          <span class="hover:text-shark-950">Register</span>
+                          <span class="hover:text-shark-950 font-urbanist text-[16px]">Register</span>
                         </div>
                       </div>
                     </div>
